7faa3cc7b59e1977568d364f78f0ccaf6e9a9ddc
wassim201 <wassim.kes213@gmail.com>
2020-10-13 12:26:32 +0200

    Create rpn.py

---
+++ rpn.py
@@ -0,0 +1,35 @@
+import re
+from collections import deque
+# operands, operators = [], []
+
+print('Reverse Polish Notation\n')
+
+expression = input("Enter a mathematical expression :\n").split()
+print(expression)
+# operands = [re.findall(r'\d+', expression)]
+# operators = [re.findall(r'\D+', expression)]
+
+
+def eval(liste):
+    var = deque(liste)
+    result = 0
+
+    op1 = int(var.popleft())
+
+    op2 = int(var.popleft())
+    operation = var.popleft()
+    if (operation == '*'):
+        result = op1 * op2
+    if (operation == '+'):
+        result = op1 + op2
+    if (operation == '-'):
+        result = op1 - op2
+    if (operation == '/'):
+        result = op1 / op2
+    var.appendleft(str(result))
+    return list(var)
+
+
+while (len(expression) > 1):
+    expression = eval(expression)
+print(expression[0])
